ec4e372c754eb2743ec3abeb1f3fcb779a079b70
johnotieno0 <poptiff@gmail.com>
2024-05-29 09:21:55 +0300

    docs: remove unwanted comments

--- Lib/checkInput.go
+++ Lib/checkInput.go
@@ -10,61 +10,19 @@ func CheckInput(input []string) (string, string, string, string) {
 		mainString = input[0]
 		subString = input[0]
 		color, reset = colorPicker(colorFlag)
-		// fmt.Printf("main : %s sun : %s  color : %s reset : %s", mainString, subString, color, reset)
-		// printOut(color, reset, mainString, subString)
 	} else if len(input) == 2 {
 		colorFlag = input[0]
 		mainString = input[1]
 		subString = input[1]
 		color, reset = colorPicker(colorFlag)
-		// printOut(color, reset, mainString, subString)
 	} else if len(input) == 3 {
 		colorFlag = input[0]
 		subString = input[1]
 		mainString = input[2]
-		// for _, ch := range mainString {
-		// 	if !matchingStr(subString, string(ch)) {
-		// 		fmt.Printf("Error: %s has no match\n", subString)
-		// 		os.Exit(0)
-		// 	}
-		// }
 		color, reset = colorPicker(colorFlag)
-		// printOut(color, reset, mainString, subString)
 	}
 	return color, reset, mainString, subString
 }
-
-// func printOut(color string, reset string, mainString string, subString string) {
-// 	match, found := matchingStr(mainString, subString)
-// 	if !found {
-// 		fmt.Printf("Error: %s has no match\n", subString)
-// 		os.Exit(0)
-// 	}
-// 	// for _, ch := range mainString {
-// 	// 	if matchingStr(subString, string(ch)) && !match[string(ch)] {
-// 	// 		match[string(ch)] = true
-// 	// 	} else if !matchingStr(subString, string(ch)) {
-// 	// 		match[string(ch)] = false
-// 	// 	}
-// 	// 	// else if !matchingStr(subString, string(ch)) && i == len(mainString) -1 {
-// 	// 	// 	fmt.Printf("Error: %s has no match\n", subString)
-// 	// 	// 	os.Exit(0)
-// 	// 	// }
-// 	// }
-// 	for _, ch := range mainString {
-// 		if match[string(ch)] {
-// 			fmt.Printf("%s%s%s", color, string(ch), reset)
-// 		} else {
-// 			fmt.Printf("%s", string(ch))
-// 			// else if matchingStr(subString, string(ch)) && match[string(ch)] {
-// 			// 	fmt.Printf("%s%s%s", color, string(ch), reset)
-// 			// } else if !matchingStr(subString, string(ch)) && !match[string(ch)] {
-// 			// 	fmt.Printf("%s", string(ch))
-// 			// }
-// 		}
-// 	}
-// 	fmt.Println()
-// }
 
 func matchingStr(s string, subString string) (map[string]bool, bool) {
 	match := make(map[string]bool)
