c49fd02cd3960b7f7554ada146baea85f49f64c5
nyunja <poptiff@gmail.com>
2024-06-11 10:28:59 +0300

    update: removed commented code in subToColor

--- Lib/SubFunctions.go
+++ Lib/SubFunctions.go
@@ -126,10 +126,6 @@ func trimSpace(s string) string {
 // Returns range of substring to be colored and true if a match is found
 func subToColor(s string, subStrings []string) (bool, int, int) {
 	for _, sub := range subStrings {
-		// if j <= len(s)-len(sub) && s[j:j+len(sub)] == sub {
-		// 	fmt.Printf("%d %d\n", j, j + len(sub) - 1)
-		// 	return true, j, j + len(sub) - 1
-		// }
 		for i := 0; i < len(s); i++ {
 			if i <= len(s)-len(sub) && s[i:i+len(sub)] == sub {
 				return true, i, i + len(sub) - 1
