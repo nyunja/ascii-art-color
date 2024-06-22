8f1c17634162a169cfd654b2f72def856ed65da1
nyunja <poptiff@gmail.com>
2024-06-22 16:58:24 +0300

    refactor checkinput to use structs to make it more scalable and maintainable

--- Lib/checkInput.go
+++ Lib/checkInput.go
@@ -1,5 +1,10 @@
 package Lib
 
+type flagz struct {
+	s string
+	t string
+}
+
 func CheckInput(input []string) (string, string, string, string, string) {
 	// Initialize utility variables
 	colorFlag := ""
@@ -12,67 +17,108 @@ func CheckInput(input []string) (string, string, string, string, string) {
 	// Trim spaces from input
 	input = inputTrimSpace(input, trimSpace)
 
-	// Check if flags are valid
-	hasValidFlag(input)
+	flg := []flagz{}
 
-	// Check the number of input arguments
-	if len(input) == 1 {
-		if isColorFlag(input[0]) {
-			PrintError()
-		}
-		// If argument is has no color flag it is used as main string
-		mainString = input[0]
-		subString = input[0]
-	} else if len(input) == 2 {
-		// If first argument is a valid color flag, assign it to colorFlag
-		if isColorFlag(input[0]) {
-			colorFlag = input[0]
-			mainString = input[1]
-			subString = input[1]
-			// If second argument is a valid banner file assign it as fileName, add .txt extension
-		} else if isBanner(input[1]) {
-			mainString = input[0]
-			subString = input[0]
-			bannerFile = input[1] + ".txt"
-			// if first argument is a valid banner file assign it as file name, add .txt extension
+	for _, str := range input {
+		if isFlag(str) {
+			flg = append(flg, flagz{str, "f"})
 		} else {
-			PrintError()
-		}
-	} else if len(input) == 3 {
-		// If first argument is a valid color flag, assign it to colorFlag
-		if isColorFlag(input[0]) {
-			colorFlag = input[0]
-			// If third argument is a valid banner file assign it as fileName, add .txt extension
-			if isBanner(input[2]) {
-				mainString = input[1]
-				subString = input[1]
-				bannerFile = input[2] + ".txt"
-				// Check if user wants to use standard, shadow and thinkertoy as main or sub string
-			} else if input[2] == "\\standard" || input[2] == "\\shadow" || input[2] == "\\thinkertoy" {
-				mainString = input[2][1:]
-				subString = input[1]
-			} else {
-				mainString = input[2]
-				subString = input[1]
-			}
-			// If first argument is a valid banner file assign it as file name, add .txt extension
-		} else {
-			PrintError()
-		}
-	} else if len(input) == 4 {
-		// If first argument is a valid color flag, assign it to colorFlag
-		if isColorFlag(input[0]) {
-			colorFlag = input[0]
-			// If second argument is a valid banner file assign it as fileName, add .txt extension
-			if isBanner(input[3]) {
-				mainString = input[2]
-				subString = input[1]
-				bannerFile = input[3] + ".txt"
-			}
-		} else {
-			PrintError()
+			flg = append(flg, flagz{str, "s"})
 		}
 	}
+
+	for i, item := range flg {
+		if item.t == "f" {
+			if isColorFlag(item.s) {
+				colorFlag = item.s
+				flg = append(flg[:i], flg[i+1:]...)
+			} else {
+				PrintError()
+			}
+		} else if i == len(flg)-1 && item.t == "s" {
+			if isBanner(item.s) {
+				bannerFile = item.s + ".txt"
+				flg = flg[:i]
+			}
+		}
+	}
+
+	if len(flg) < 1 || len(flg) > 2 {
+		PrintError()
+	}
+	
+	if len(flg) == 2 {
+		if flg[1].s == "\\standard" || flg[1].s == "\\shadow" || flg[1].s == "\\thinkertoy" {
+			flg[1].s = flg[1].s[1:]
+		}
+		mainString = flg[1].s
+		subString = flg[0].s
+	} else if len(flg) == 1 {
+		mainString = flg[0].s
+		subString = flg[0].s
+	}
+
+	// // Check if flags are valid
+	// hasValidFlag(input)
+
+	// // Check the number of input arguments
+	// if len(input) == 1 {
+	// 	if isColorFlag(input[0]) {
+	// 		PrintError()
+	// 	}
+	// 	// If argument is has no color flag it is used as main string
+	// 	mainString = input[0]
+	// 	subString = input[0]
+	// } else if len(input) == 2 {
+	// 	// If first argument is a valid color flag, assign it to colorFlag
+	// 	if isColorFlag(input[0]) {
+	// 		colorFlag = input[0]
+	// 		mainString = input[1]
+	// 		subString = input[1]
+	// 		// If second argument is a valid banner file assign it as fileName, add .txt extension
+	// 	} else if isBanner(input[1]) {
+	// 		mainString = input[0]
+	// 		subString = input[0]
+	// 		bannerFile = input[1] + ".txt"
+	// 		// if first argument is a valid banner file assign it as file name, add .txt extension
+	// 	} else {
+	// 		PrintError()
+	// 	}
+	// } else if len(input) == 3 {
+	// 	// If first argument is a valid color flag, assign it to colorFlag
+	// 	if isColorFlag(input[0]) {
+	// 		colorFlag = input[0]
+	// 		// If third argument is a valid banner file assign it as fileName, add .txt extension
+	// 		if isBanner(input[2]) {
+	// 			mainString = input[1]
+	// 			subString = input[1]
+	// 			bannerFile = input[2] + ".txt"
+	// 			// Check if user wants to use standard, shadow and thinkertoy as main or sub string
+	// 		} else if input[2] == "\\standard" || input[2] == "\\shadow" || input[2] == "\\thinkertoy" {
+	// 			mainString = input[2][1:]
+	// 			subString = input[1]
+	// 		} else {
+	// 			mainString = input[2]
+	// 			subString = input[1]
+	// 		}
+	// 		// If first argument is a valid banner file assign it as file name, add .txt extension
+	// 	} else {
+	// 		PrintError()
+	// 	}
+	// } else if len(input) == 4 {
+	// 	// If first argument is a valid color flag, assign it to colorFlag
+	// 	if isColorFlag(input[0]) {
+	// 		colorFlag = input[0]
+	// 		// If second argument is a valid banner file assign it as fileName, add .txt extension
+	// 		if isBanner(input[3]) {
+	// 			mainString = input[2]
+	// 			subString = input[1]
+	// 			bannerFile = input[3] + ".txt"
+	// 		}
+	// 	} else {
+	// 		PrintError()
+	// 	}
+	// }
 	// Extract color and reset codes from colorFlag
 	color, reset = colorPicker(colorFlag)
 
--- Lib/checkInputHelper.go
+++ Lib/checkInputHelper.go
@@ -30,16 +30,30 @@ func isColorFlag(s string) bool {
 	return len(s) > 8 && hasPrefix(s, "--color=")
 }
 
+func isFlag(s string) bool {
+	return hasPrefix(s, "-") || hasPrefix(s, "--")
+}
+
 // Check if flag format is valid
 func hasValidFlag(arr []string) {
 	for _, s := range arr {
-		if hasPrefix(s, "-") || hasPrefix(s, "--") {
+		if isFlag(s) {
 			if !isColorFlag(s) {
 				PrintError()
 			}
 		}
 	}
 }
+
+// Check if flag format is valid
+// func markFlag(arr []string) string {
+// 	for _, s := range arr {
+// 		if isFlag(s) {
+// 			return "f"
+// 		}
+// 	}
+// 	return "s"
+// }
 
 func isBanner(s string) bool {
 	if s == "standard" || s == "shadow" || s == "thinkertoy" {
