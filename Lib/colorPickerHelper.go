c8141656d41d6b4091b731a49e88eb0e54fed33a
davodhiambo <jesseomolo@yahoo.com>
2024-07-01 14:06:10 +0300

    documentation: add comments to colorPickerHelper.go

--- Lib/colorPickerHelper.go
+++ Lib/colorPickerHelper.go
@@ -4,6 +4,7 @@ package Lib
 func toLower(s string) string {
 	res := ""
 	for _, ch := range s {
+		// match capital letters and convert them to lowercase
 		if ch >= 'A' && ch <= 'Z' {
 			res += string(ch + 32)
 		} else {
