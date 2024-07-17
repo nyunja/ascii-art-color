e5cc1d53a7b40f741676d6fc642c43782648db31
Nyunja <poptiff@gmail.com>
2024-07-17 08:23:44 +0300

    feat: added counter to track indexex. feat: removed the use of []Output instead the mainstring is split

--- Lib/Printer.go
+++ Lib/Printer.go
@@ -3,44 +3,60 @@ package Lib
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func getFinalOutput(color1, color2, reset, mainStr, subStr string, lines []string) string {
 	var output string
-	var startIdx int
+	var startIdx, charCount, countNewlines int
 
-	outputStructSlice := getOutputSlice(mainStr)      // Extract characters to be printed
+	//outputStructSlice := getOutputSlice(mainStr)      // Extract characters to be printed
 	indicesToColor := indicesToColor(mainStr, subStr) // Extract indices that need coloring
+	fmt.Println(indicesToColor)
 
 	// Populate output with ascii-art
-	for _, outputStruct := range outputStructSlice {
-		if outputStruct.newLine == "\n" {
-			output += "\n"
+	words := strings.Split(mainStr, "\n")
+	fmt.Printf("%q\n", words)
+	for _, word := range words {
+		if word == "" {
+			countNewlines++
+			if countNewlines < len(words) {
+				output += "\n"
+				charCount++
+				fmt.Println("newline: ", charCount)
+			}
 		} else {
-			word := outputStruct.word
+			//word := word.word
 			if len(word) > 0 {
 				// Populate output with every line of ascii-art
 				for k := 0; k < 8; k++ {
 					for _, ch := range word {
-						startIdx = int(ch.character-32)*9 + 1
+						startIdx = int(ch-32)*9 + 1
 
 						// Append color code for color1 to appropriate characters
-						if len(color1) > 0 && indicesToColor[ch.index] {
+						if len(color1) > 0 && indicesToColor[charCount] {
+							fmt.Println("true: ", charCount)
 							output += color1 + lines[startIdx+k] + reset
+							charCount++
 
 							// if true, execute color code of color2
 						} else if len(color2) > 0 {
 							output += color2 + lines[startIdx+k] + reset
+							charCount++
 
 							// if no index to color, append ascii art without color codes
 						} else {
 							output += lines[startIdx+k]
+							charCount++
 						}
 					}
+					charCount = charCount - len(word)
+					fmt.Println(charCount)
 
 					// Add new-line-character at the end of each line of output
 					output += "\n"
 				}
+				charCount = charCount + len(word)
 			}
 		}
 	}
